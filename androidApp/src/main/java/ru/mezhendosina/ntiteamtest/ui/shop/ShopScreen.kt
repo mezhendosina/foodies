7e2b922446a28c590b31a98594f9ea8c541b2e27
mezhendosina <mensh.zheya@gmail.com>
2024-05-16 11:05:50 +0300

    feat: replace LazyRow in top with TabRow and add HorizontalPager

--- androidApp/src/main/java/ru/mezhendosina/ntiteamtest/ui/shop/ShopScreen.kt
+++ androidApp/src/main/java/ru/mezhendosina/ntiteamtest/ui/shop/ShopScreen.kt
@@ -1,118 +1,114 @@
 package ru.mezhendosina.ntiteamtest.ui.shop
 
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.slideInVertically
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.airbnb.lottie.compose.LottieAnimation
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
-import org.koin.compose.getKoin
-import org.koin.core.context.GlobalContext.get
+import kotlinx.coroutines.launch
 import ru.mezhendosina.ntiteamtest.R
 import ru.mezhendosina.ntiteamtest.ui.components.Category
-import ru.mezhendosina.ntiteamtest.ui.components.EmptyScreen
 import ru.mezhendosina.ntiteamtest.ui.components.FixedButton
-import ru.mezhendosina.ntiteamtest.ui.components.ItemCard
 import ru.mezhendosina.ntiteamtest.ui.theme.NtiTeamTestTheme
-import ru.mezhendosina.shared.ui.entities.UiState
 import ru.mezhendosina.shared.ui.shop.PreviewShopComponent
 import ru.mezhendosina.shared.ui.shop.ShopComponent
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun ShopScreen(component: ShopComponent) {
     val model by component.model.subscribeAsState()
 
-    Scaffold(topBar = {
-        Column {
-            CenterAlignedTopAppBar(
-                title = {
-                    Image(
-                        painterResource(id = R.drawable.ic_logo),
-                        null
-                    )
-                },
-                colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.background)
-            )
-            AnimatedVisibility(
-                visible = model.category.isNotEmpty(),
-                enter = slideInVertically() + fadeIn(),
+    val pagerState = rememberPagerState {
+        model.category.size
+    }
 
+    LaunchedEffect(pagerState.currentPage) {
+        if (model.category.size > 0) {
+            component.onCategoryClick(model.category[pagerState.currentPage].id)
+        }
+    }
+
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        topBar = {
+            Column {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Image(
+                            painterResource(id = R.drawable.ic_logo), null
+                        )
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.background)
+                )
+                AnimatedVisibility(
+                    visible = model.category.isNotEmpty(),
+                    enter = slideInVertically() + fadeIn(),
                 ) {
-                LazyRow(modifier = Modifier.padding(horizontal = 8.dp)) {
-                    items(model.category) {
-                        Category(name = it.name, enabled = it.id == model.selectedCategoryId) {
-                            component.onCategoryClick(it.id)
+                    ScrollableTabRow(
+                        selectedTabIndex = pagerState.currentPage,
+                        indicator = {},
+                        containerColor = MaterialTheme.colorScheme.background,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 8.dp),
+                        divider = {}
+                    ) {
+                        model.category.forEachIndexed { index, categoryEntity ->
+                            Category(
+                                name = categoryEntity.name,
+                                enabled = categoryEntity.id == model.category[pagerState.currentPage].id
+                            ) {
+                                component.onCategoryClick(categoryEntity.id)
+                                coroutineScope.launch {
+                                    pagerState.animateScrollToPage(index)
+                                }
+                            }
                         }
                     }
                 }
             }
+        },
+        bottomBar = {
+            FixedButton(
+                text = stringResource(R.string.rubs, model.cartSum),
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp),
+                model.cartSum > 0,
+                onCLick = component::onCartClick
+            )
         }
-
-    }, bottomBar = {
-        FixedButton(
-            text = stringResource(R.string.rubs, model.cartSum),
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp),
-            model.cartSum > 0,
-            onCLick = component::onCartClick
-        )
-    }) { paddingValues ->
-        AnimatedContent(targetState = model.state) {
-            if (it == UiState.LOADING) {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    CircularProgressIndicator()
-                }
-                return@AnimatedContent
-            } else if (model.items.isEmpty() && model.state == UiState.LOADED) {
-                EmptyScreen(text = stringResource(R.string.emty_catalog))
-                return@AnimatedContent
-            }
-            LazyVerticalGrid(
-                columns = GridCells.Adaptive(167.dp),
-                modifier = Modifier
-                    .animateContentSize()
-                    .padding(paddingValues)
-                    .padding(horizontal = 8.dp),
-            ) {
-                items(model.items) { itemEntity ->
-                    ItemCard(
-                        itemEntity = itemEntity,
-                        onClick = { component.onItemClick(itemEntity.id) }) {
-                        component.onItemCountChanges(itemEntity.id, it)
-                    }
-                }
-            }
+    ) { paddingValues ->
+        HorizontalPager(state = pagerState) {
+            ShopItem(
+                model.items,
+                model.state,
+                paddingValues,
+                component::onItemClick,
+                component::onItemCountChanges
+            )
         }
     }
 }
